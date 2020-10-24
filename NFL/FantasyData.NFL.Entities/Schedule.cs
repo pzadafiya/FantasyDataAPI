ba73e31da444a9465cdbe67fc888d17c81d6ac37
Pranav <Pranav@orionswave.com>
2020-10-24 17:50:09 +0530

    Add some filed in score, playerGame and schedule  entites in NFL Api.

--- NFL/FantasyData.NFL.Entities/Schedule.cs
+++ NFL/FantasyData.NFL.Entities/Schedule.cs
@@ -6,43 +6,45 @@ using System.Threading.Tasks;
 
 namespace FantasyData.NFL.Entities
 {
-    public class Schedules : List<Schedule>
-    { }
+	public class Schedules : List<Schedule>
+	{ }
 
-    public class Schedule
-    {
-        public Schedule()
-        {
-            StadiumDetails = new StadiumDetails();
-        }
-        public string AwayTeam { get; set; }
-        public string Channel { get; set; }
-        public DateTime? Date { get; set; }
-        public string GameKey { get; set; }
-        public string HomeTeam { get; set; }
-        public double OverUnder { get; set; }
-        public double PointSpread { get; set; }
-        public int Season { get; set; }
-        public int SeasonType { get; set; }
-        public int StadiumID { get; set; }
-        public int Week { get; set; }
+	public class Schedule
+	{
+		public Schedule()
+		{
+			StadiumDetails = new StadiumDetails();
+		}
+		public string AwayTeam { get; set; }
+		public string Channel { get; set; }
+		public DateTime? Date { get; set; }
+		public string GameKey { get; set; }
+		public string HomeTeam { get; set; }
+		public double OverUnder { get; set; }
+		public double PointSpread { get; set; }
+		public int Season { get; set; }
+		public int SeasonType { get; set; }
+		public int StadiumID { get; set; }
+		public int Week { get; set; }
 
-        public bool? Canceled { get; set; }
-        public double GeoLat { get; set; }
-        public double GeoLong { get; set; }
-        public int ForecastTempLow { get; set; }
-        public int ForecastTempHigh { get; set; }
-        public string ForecastDescription { get; set; }
-        public int ForecastWindChill { get; set; }
-        public int ForecastWindSpeed { get; set; }
-        public int AwayTeamMoneyLine { get; set; }
-        public int HomeTeamMoneyLine { get; set; }
-        public DateTime? Day { get; set; }
-        public DateTime? DateTime { get; set; }
-        public int GlobalGameID { get; set; }
-        public int GlobalAwayTeamID { get; set; }
-        public int GlobalHomeTeamID { get; set; }
-        public int ScoreID { get; set; }
-        public StadiumDetails StadiumDetails { get; set; }
-    }
+		public bool? Canceled { get; set; }
+		public double GeoLat { get; set; }
+		public double GeoLong { get; set; }
+		public int ForecastTempLow { get; set; }
+		public int ForecastTempHigh { get; set; }
+		public string ForecastDescription { get; set; }
+		public int ForecastWindChill { get; set; }
+		public int ForecastWindSpeed { get; set; }
+		public int AwayTeamMoneyLine { get; set; }
+		public int HomeTeamMoneyLine { get; set; }
+		public DateTime? Day { get; set; }
+		public DateTime? DateTime { get; set; }
+		public int GlobalGameID { get; set; }
+		public int GlobalAwayTeamID { get; set; }
+		public int GlobalHomeTeamID { get; set; }
+		public int ScoreID { get; set; }
+		public string Status { get; set; }
+		public StadiumDetails StadiumDetails { get; set; }
+
+	}
 }
